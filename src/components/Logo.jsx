8561326696aa75af55a6f1ad5eac5cc206656762
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-01-08 15:06:11 -0300

    feat: add Logo Component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import komeiLogo from '../assets/img/logo/komei_juku.webp'
+
+const Logo = () => {
+
+    return (
+        <a class="navbar-brand " href="#"><img src={komeiLogo} alt="Logo de la escuela" /></a>
+    )
+
+}
+
+export default Logo
