5a08dd82efa5f020ad1983aea8ac2b861d2c4676
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-01-08 15:17:33 -0300

    feat: add navbar products

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,13 @@
-
+import Productos from './Productos'
 
 const Nav = () => {
 
     return (
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Nosotros</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active text-center text-sm-end" aria-current="page" href="./views/tienda.html">Tienda</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Dojos</a>
-            </li>
+            <li class="nav-item"><a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Nosotros</a></li>
+            <li class="nav-item"><a class="nav-link active text-center text-sm-end dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a><Productos /></li>
+            <li class="nav-item"><a class="nav-link active text-center text-sm-end" aria-current="page" href="#">Dojos</a></li>
         </ul>
     )
 }
---
+++ src/components/Productos.jsx
@@ -0,0 +1,15 @@
+
+
+const Productos = () => {
+
+    return (
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item text-capitalize" href="#">Catalogo Completo</a></li>
+            <li><a class="dropdown-item text-capitalize" href="#">Clases</a></li>
+            <li><a class="dropdown-item text-capitalize" href="#">Ropa</a></li>
+            <li><a class="dropdown-item text-capitalize" href="#">Equipos</a></li>
+        </ul>
+    )
+}
+
+export default Productos
